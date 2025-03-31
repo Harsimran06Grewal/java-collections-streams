ca89aa42d03ef7991f27fc1ec9bd58a41158ec0a
Harsimran Kaur <harsimran0596.be21@chitkara.edu.in>
2025-03-31 13:07:46 +0530

    [Harsimran] add : Testing a Calculator class

---
+++ jUnit/src/main/BasicTesting/Calculator.java
@@ -0,0 +1,27 @@
+package BasicTesting;
+
+public class Calculator {
+    public static void main(String[] args) {
+        // Main Method
+    }
+
+    // Adds two numbers
+    public static int addition(int a, int b){
+        return a + b;
+    }
+
+    // Subtracts second number from first
+    public static int subtract(int a, int b){
+        return a - b;
+    }
+
+    // Multiplies two numbers
+    public static int multiply(int a, int b){
+        return a * b;
+    }
+
+    // Divides first number by second
+    public static int divide(int a, int b){
+        return a / b;
+    }
+}
